b5991d7831a8661c076db2730f00b83f1cab560b
Gchin99 <gchin99@gmail.com>
2022-07-23 18:06:09 -0700

    completed loop challenges with while loops

--- script.js
+++ script.js
@@ -7,13 +7,14 @@
 //     console.log(i);
 // }
 
-var i = 1;
+// var i = 1;
 
-while(i<20) {
-        i % 2 == 1;
-        console.log(i);
-        i++;
-}
+// while(i<20) {
+//         if (i % 2 == 1){
+//         console.log(i);
+//         }
+//         i++;
+// }
 
 
 //***2. Decreasing Multiples of 3: Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
@@ -23,6 +24,15 @@ while(i<20) {
 //     console.log (i);
 // }
 
+// var i = 100;
+
+// while(i>0) {
+//         if (i % 3 ==0){
+//                 console.log(i);
+//         }
+//         i--;
+// }
+
 
 //3. Print the sequence:  Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
 
@@ -30,21 +40,32 @@ while(i<20) {
 //     console.log(i);
 // }
 
-// ****4.Sigma: Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the 
+
+// var i=4;
+//
+// while (i>-4){
+//         console.log(i);
+//         i-=1.5;
+// }
+
+
+// 4.Sigma: Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the 
 // result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
 
 // var x = 0;
-
+//
 // for (var i=1; i<=100; i++){
 //     x += i;
 // }
 // console.log(x); 
-//
-// var x = 1;   //this while loop did not work
-// var i = 1;
 
+
+//
+// var x = 0;   
+// var i = 1;
+//
 // while (i<101) {
-//     x + i = x;
+//     x += i;
 //     i++;
 // }
 // console.log(x)
@@ -54,9 +75,19 @@ while(i<20) {
 // at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
 
 // var x = 1;
-
+//
 // for (var i=1; i<=12; i++) {
 //     x *= i;
 // }
+//
+// console.log(x);
 
+
+// var x = 1;
+// var i = 1;
+//
+// while(i<=12){
+//         x *= i;
+//         i++;
+// }
 // console.log(x);
